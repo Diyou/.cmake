d068a22e4fcdf9d30032710a402fb49d70482230
Diyou <diyou.dev@gmail.com>
2025-07-14 08:12:06 +0200

    Put Project.json parser in its own file

--- ToolChains/common.cmake
+++ ToolChains/common.cmake
@@ -5,7 +5,6 @@ endif()
 set(${SCRIPT_NAME}_INCLUDED ON)
 
 list(APPEND CMAKE_MODULE_PATH
-    ${CMAKE_CURRENT_LIST_DIR}/../Packages
     ${CMAKE_CURRENT_LIST_DIR}/../Modules
 )
 
@@ -19,42 +18,3 @@ file(GLOB includes ${CMAKE_CURRENT_LIST_DIR}/../Includes/*.cmake)
 foreach(in ${includes})
     include(${in})
 endforeach()
-
-if(NOT EXISTS ${PROJECT_ROOT}/Project.json)
-    message(FATAL_ERROR "/Project.json missing")
-endif()
-
-file(READ ${PROJECT_ROOT}/Project.json JSON)
-
-GetJSON("${JSON}" Project DOTCMAKE_PROJECT_JSON)
-if(NOT DOTCMAKE_PROJECT_JSON)
-    message(FATAL_ERROR "Project node missing in Project.json")
-endif()
-GetJSON("${DOTCMAKE_PROJECT_JSON}" Name         DOTCMAKE_PROJECT_NAME)
-GetJSON("${DOTCMAKE_PROJECT_JSON}" ID           DOTCMAKE_PROJECT_ID)
-GetJSON("${DOTCMAKE_PROJECT_JSON}" Version      DOTCMAKE_PROJECT_VERSION)
-GetJSON("${DOTCMAKE_PROJECT_JSON}" Description  DOTCMAKE_PROJECT_DESCRIPTION)
-GetJSON("${DOTCMAKE_PROJECT_JSON}" URL          DOTCMAKE_PROJECT_URL)
-
-if(NOT DOTCMAKE_PROJECT_NAME
-OR NOT DOTCMAKE_PROJECT_ID
-OR NOT DOTCMAKE_PROJECT_VERSION)
-    message(FATAL_ERROR "Missing required Values in Project.json")
-endif()
-
-CacheString(DOTCMAKE_PROJECT_NAME        "Name value from Project.json")
-CacheString(DOTCMAKE_PROJECT_ID          "ID value from Project.json")
-CacheString(DOTCMAKE_PROJECT_VERSION     "Version value from Project.json")
-CacheString(DOTCMAKE_PROJECT_DESCRIPTION "Description value from Project.json")
-CacheString(DOTCMAKE_PROJECT_URL         "URL value from Project.json")
-
-# Filling in defaults
-if(NOT PROJECT_VERSION)
-    set(PROJECT_VERSION "${DOTCMAKE_PROJECT_VERSION}")
-endif()
-if(NOT PROJECT_DESCRIPTION)
-    set(PROJECT_DESCRIPTION "${DOTCMAKE_PROJECT_DESCRIPTION}")
-endif()
-if(NOT PROJECT_HOMEPAGE_URL)
-    set(PROJECT_HOMEPAGE_URL "${DOTCMAKE_PROJECT_URL}")
-endif()
